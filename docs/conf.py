8185283e8a63f9aa9fab5c523500257a152499fe
MinchinWeb <w_minchin@hotmail.com>
2015-01-17 19:53:09 -0700

    Add Google Analytics to documentation pages

--- docs/conf.py
+++ docs/conf.py
@@ -122,7 +122,8 @@ html_theme = 'cloud'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-html_theme_options = { "roottarget": "index" }
+html_theme_options = {"roottarget": "index",
+                      "googleanalytics_id": "UA-384291-3"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
